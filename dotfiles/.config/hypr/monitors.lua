2e3f95576289eca3b31f7a535a7c9a33e928f1a4
Noro18 <zeroxg534@gmail.com>
2026-06-02 10:19:09 +0900

    fix monitors

--- dotfiles/.config/hypr/monitors.lua
+++ dotfiles/.config/hypr/monitors.lua
@@ -1,7 +1,6 @@
 hl.monitor({
-		output = "",
-		mode = "preferred",
-		position = "auto",
-		scale = "auto",
-		reserved_area = { top = -10, bottom = 0, left = 0, right = 0 }
-})       
+	output = "",
+	mode = "1920x1080",
+	position = "auto",
+	reserved_area = { top = -10, bottom = 0, left = 0, right = 0 },
+})
